e932c88781559c0d17e8b011821d790fe7a4336e
John Wood <113411200+john-h-wood@users.noreply.github.com>
2022-12-03 00:05:05 -0500

    Some changes

--- research/frac_below_april_2022/frac_below_april_22.py
+++ research/frac_below_april_2022/frac_below_april_22.py
@@ -1,30 +1,24 @@
 import edcl as di
 
-# === Info save ===
+# ============ SETUP ===================================================================================================
 limits, projection = di.get_defaults()
 
-# winter_data = di.get_data_collection_names('ERA5', 'Wind spd (m/s)', limits, (None, None, None, None))
-# di.save_pickle(winter_data, '../pickles/winter_data.pickle')
-#
-# april_2022_data = di.get_data_collection_names('ERA5', 'Wind spd (m/s)', limits, (2022, 4, None, None))
-# di.save_pickle(april_2022_data, '../pickles/april_2022_data.pickle')
+# ============ SAVE DATA ===============================================================================================
+print('Saving data...')
 
-# === load data ===
-winter_data = di.load_pickle('../pickles/winter_data.pickle')
-# april_2022_data = di.load_pickle('../pickles/april_2022_data.pickle')
-print('data loaded')
+# Un-ordered winter data
+winter_data = di.get_data_collection_names('ERA5', 'Wind spd (m/s)', limits, (None, None, None, None))
+di.save_pickle(winter_data, '/Volumes/My Drive/SURF/pickles/winter_data.pickle')
+print('Saved unordered winter data')
 
-# === sort data ===
-# winter_data.time_order()
-# di.save_pickle(winter_data, '../pickles/winter_data.pickle')
-print(winter_data.get_time_length(), winter_data.spread)
+# Ordered winter data
+winter_data.time_order()
+di.save_pickle(winter_data, '/Volumes/My Drive/SURF/pickles/ordered_winter_data.pickle')
+print('Saved ordered winter data')
 
-# === Frac below ===
-# frac_below_april_2022 = di.fraction_below_ordered(april_2022_data, winter_data)
-# di.save_pickle(frac_below_april_2022, '../pickles/frac_below_april_2022.pickle')
+# Non-nan count
+non_nan_count = di.count_non_nan(winter_data)
+di.save_pickle(non_nan_count, '/Volumes/My Drive/SURF/pickles/non_nan_count_winter.pickle')
+print('Saved winter non-nan count')
 
-# === check specific time ===
-spec_data = di.get_data_collection_names('ERA5', 'Wind spd (m/s)', limits, (2022, 4, 19, 12))
-results = di.fraction_below_ordered(spec_data, winter_data)
-di.plot_graphables(results, 'heat_jet', projection, None, (0.9, 0.92, 0.94, 0.96, 0.98, 1.), None, None, 'save',
-                   None, 'plot.png', 12)
+# ============ APRIL 2022 FRAC BELOW ===================================================================================
