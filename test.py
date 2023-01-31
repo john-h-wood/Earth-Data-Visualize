186788f5353579581e8d9f36c519a26e93b1510f
John Wood <113411200+john-h-wood@users.noreply.github.com>
2023-01-31 03:01:20 -0500

    Automatic limits added. Passing limits=None is get_data_collection/get_data_collection_names yields data from all available coordinates. Limits are computed and saved by DataCollection class.

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import edcl as di
-from scipy.io import loadmat
 
-d = loadmat('/Volumes/My Drive/Moore/data/arr/2022/arr_mslp_m02_y2022_cf_6h.mat')
+
+data_new = di.get_data_collection_names('ERA5', 'Wind spd (m/s)', None, (2022, 4, 19, 12))
+projection = di.get_projection_name('Lambert', data_new.get_limits())
+
+di.plot_graphables(data_new, 'heat_jet', projection, None, None, None, (12, 8), None, 'save', None, 'ex.png', 12)
+
+
 
 
